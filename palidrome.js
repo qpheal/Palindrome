567e8d8662d4f0acd50ef87ccba3711949a8ff52
Adjei Rapheal <ralphrapheals@gmail.com>
2024-02-17 00:53:13 +0000

    Third Commit

--- palidrome.js
+++ palidrome.js
@@ -2,8 +2,8 @@ function palindrome(string){
     if (string === string.split("").reverse().join(""))
     {
         return true
-    } 
+    } else return false
 }
 
 const text = palindrome("haruah")
-console.log(text)
+console.log(text) // false
